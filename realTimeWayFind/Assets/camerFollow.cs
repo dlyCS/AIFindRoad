7343a8020ad191685e0b66a383573ea5101ace7c
qydxr <75769571+qydxr@users.noreply.github.com>
2020-12-18 23:16:16 +0800

    淦

---
+++ realTimeWayFind/Assets/camerFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerFollow : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("sun1").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y,-10f);
+    }
+}
---
+++ realTimeWayFind/Assets/move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Transform player;
+    public Vector2 moveTraget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform;
+        moveTraget = player.position;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButton(0)||Input.GetMouseButton(1))
+        {
+
+            moveTraget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+        }
+
+        Vector2 pos = Vector2.MoveTowards(this.player.position, moveTraget, speed * Time.deltaTime);
+
+        this.player.position = pos;
+       
+    }
+}
